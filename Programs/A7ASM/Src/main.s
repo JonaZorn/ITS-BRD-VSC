01a349bc304a2d5de75f4ffc8329a32029fe5fc9
JonaZorn <zornjona@gmail.com>
2026-06-11 15:28:50 +0200

    Aufgabe7

--- Programs/A7ASM/Src/main.s
+++ Programs/A7ASM/Src/main.s
@@ -93,13 +93,24 @@ superloop
 ; Timer leesen
 		ldr		R1,=TIMER
 		ldr		R0,[R1]		; 1 tick = 10us
-		muv		R2,#100		
-		udiv	R1,R0,R2	; in Millisekunden
+		mov		R2,#60000000		
+		udiv	R1,R0,R2	; in 10 minuten
+		str
+		mov		R2,#6000000
+		udiv	R3,R1,R2	; in 1 Minuten
+		str
+		mov		R2,#1000000
+		udiv	R4,R3,R2	; in 10 sekunden 
+		str
+		mov		R2,#100000
+		udiv	R4,R3,R2	; in 1 sekunden
+		str
+		mov		R2,#10000
+		udiv	R4,R3,R2	; in 10 millisekunde
+		str
 		mov		R2,#1000
-		udiv	R3,R1,R2	; in Sekunden
-		mov		R2,#60
-		udiv	R4,R3,R2	; in Minuten
-
+		udiv	R4,R3,R2	; in 1 millisekunde
+		str
 
 ; read buttons
 		LDR		R0,=GPIO_F_PIN	; Liest GPIO-F-Eingangsregister 
